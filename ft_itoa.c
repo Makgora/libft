43fb12afb3f5b1cda9c71522c91178cc528560c8
Makgora <titouan.parand@outlook.fr>
2017-09-25 16:11:06 +0200

    fix itoa 0 and itoa overflow

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,13 +1,13 @@
 #include "libft.h"
 
-size_t	get_length(int n)
+size_t	get_length(long int nb)
 {
 	size_t	size;
 
 	size = 0;
-	while (n != 0)
+	while (nb > 0)
 	{
-		n /= 10;
+		nb /= 10;
 		size++;
 	}
 	return (size);
@@ -15,24 +15,29 @@ size_t	get_length(int n)
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	size_t	str_len;
-	
-	if (n < 0)
+	char		*str;
+	size_t		str_len;
+	long int	nb;
+
+	nb = (long int)n;
+	if (nb < 0)
 	{
-		str_len = get_length(n) + 1;
+		nb *= -1;
+		str_len = get_length(nb) + 1;
 		str = ft_strnew(str_len);
 		*str = '-';
 	}
 	else
 	{
-		str_len = get_length(n);
+		str_len = get_length(nb);
 		str = ft_strnew(str_len);
 	}
-	while (n != 0)
+	if (nb == 0)
+		*str = '0';
+	while (nb > 0)
 	{
-		str[str_len - 1] = n % 10 + '0';
-		n /= 10;
+		str[str_len - 1] = nb % 10 + '0';
+		nb /= 10;
 		str_len--;
 	}
 	return (str);
