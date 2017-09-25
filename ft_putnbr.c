cff6af6cd0ad1a9900b3927e8b89973edc03af60
Makgora <titouan.parand@outlook.fr>
2017-09-25 19:21:51 +0200

    name put_nbr_aux

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,15 +1,15 @@
 #include "libft.h"
 
-void	ft_putnbr_aux(long int n)
+void	putnbr_aux(long int n)
 {
 	if (n < 0)
 	{
 		ft_putchar('-');
-		ft_putnbr_aux(n * -1);
+		putnbr_aux(n * -1);
 	}
 	else if (n % 10 != 0)
 	{
-		ft_putnbr_aux(n / 10);
+		putnbr_aux(n / 10);
 		ft_putchar(n % 10 + '0');
 	}
 }
@@ -19,5 +19,5 @@ void	ft_putnbr(int n)
 	if (n == 0)
 		ft_putchar('0');
 	else
-		ft_putnbr_aux((long int)n);
+		putnbr_aux((long int)n);
 }
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,15 +1,15 @@
 #include "libft.h"
 
-void	ft_putnbr_fd_aux(long int n, int fd)
+void	putnbr_fd_aux(long int n, int fd)
 {
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd_aux(n * -1, fd);
+		putnbr_fd_aux(n * -1, fd);
 	}
 	else if (n % 10 != 0)
 	{
-		ft_putnbr_fd_aux(n / 10, fd);
+		putnbr_fd_aux(n / 10, fd);
 		ft_putchar_fd(n % 10 + '0', fd);
 	}
 }
@@ -19,5 +19,5 @@ void	ft_putnbr_fd(int n, int fd)
 	if (n == 0)
 		ft_putchar_fd('0', fd);
 	else
-		ft_putnbr_fd_aux((long int)n, fd);
+		putnbr_fd_aux((long int)n, fd);
 }
