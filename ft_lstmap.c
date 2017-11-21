827e4452fa4f430441d67dc7d85ebf8fcd3881b8
Makgora <titouan.parand@outlook.fr>
2017-11-21 09:25:12 +0100

    f

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,5 +14,11 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
+	t_list	*new;
+	if (lst == NULL)
+		return (NULL);
+	new = ft_lstnew(NULL, 0);
+	while (ft_lstislast(lst) != 1)
+
 	return (f(lst));
 }
