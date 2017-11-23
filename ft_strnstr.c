16a224ba2fa9d19e692ab983f53beb8591d97c87
Makgora <titouan.parand@outlook.fr>
2017-11-23 09:34:03 +0100

    strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -16,40 +16,36 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	const char	*p_big;
 	const char	*p_little;
-	size_t		len_bis;
+	size_t		len_b;
 
 	if (*little == '\0')
 		return ((char *)big);
-	while (*big != '\0' && len > 0)
+	while (*big != '\0')
 	{
 		p_big = big;
 		p_little = little;
-		len_bis = len;
-		while (len_bis > 0 && *p_little != '\0' && *p_little == *p_big)
+		len_b = len--;
+		while (len_b > 0 && *p_big != '\0' && *p_little == *p_big)
 		{
 			p_little++;
 			p_big++;
-			len_bis--;
+			len_b--;
 		}
-		printf("p_big: %s, p_little: %s, len: %zu\n", p_big, p_little, len_bis);
 		if (*p_little == '\0')
 			return ((char *)big);
-		while (p_big - big >= 0 && len > 0)
-		{
-			big++;
-			len--;
-		}
+		big++;
 	}
 	return (NULL);
+
 }
-/*
-**int main(int argc, const char *argv[])
-**{
-**	(void)argc;
-**	(void)argv;
-**
-**	char buf2[] = "ozarabozaraboze123";
-**	printf("|%s| - |ozaraboze123|\n", ft_strnstr(buf2, "ozaraboze", 15));
-**	return 0;
-**}
-*/
+
+int main(int argc, const char *argv[])
+{
+	(void)argc;
+	(void)argv;
+
+	char buf2[] = "ozarabozaraboze123";
+	printf("|%s| - |ozaraboze123|\n", ft_strnstr(buf2, "ozaraboze", 15));
+	return 0;
+}
+
